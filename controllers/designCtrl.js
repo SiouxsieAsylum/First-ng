81b5a2668742c67365c616ee1629a9bd58957228
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2018-02-20 17:45:33 -0500

    must fix formatting for radio input

--- controllers/designCtrl.js
+++ controllers/designCtrl.js
@@ -1,18 +1,38 @@
-app.controller('designCtrl', function($scope, $interval, $timeout, collectInfo,){
+app.controller('designCtrl', function($scope, $interval,$parse, $timeout, collectInfo,){
 	$scope.inputFields=["Age","Sex","Height","Weight","BMI","O2 Sats","Systolic BP", "Diastolic BP" ]
 	$scope.radioFields=["L Eye Vitrious Clear?","R Eye Vitrious Clear?", "L Ear Canal Clear?", "R Ear Canal Clear?"]
 	$scope.inputCounter=0
 	$scope.radioCounter=0
 	$scope.hoverEdit=false
 	$scope.buttonActivated=false
-	$scope.setActive=false
-
-	$scope.setActivityTrue=function(){
-		$scope.setActive = true
+	$scope.setActive={
+		'inputs': false,
+		'inputsHidden':false,
+		'radioHidden':false,
+		'radio':false 
 	}
+	$scope.activeForm = null
+	$scope.form = {}
+	$scope.modalActive = false
+
+	$scope.setActivityTrue=function(formname){
+		$scope.setActive[formname] = true
+		$scope.modalActive = true
+		// how to get $scope.variable
+		$scope.activeForm = $scope.$eval(formname)
+		console.log($scope.activeForm.$name)
+	}
+
 	$scope.setActivityFalse=function(){
-		$scope.setActive = false
+		$scope.setActive = {
+			'inputs': false,
+			'inputsHidden':false,
+			'radioHidden':false,
+			'radio':false 
+		}
+
 		$scope.hoverEdit = false
+		$scope.modalActive = false
 	}
 
 	$scope.aquInfo= function(feild,info){
